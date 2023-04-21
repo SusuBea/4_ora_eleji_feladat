ea0ccfc828cc5fc49ec461bc49289bc918a06079
SusuBea <suhajdabea@gmail.com>
2023-04-21 22:04:33 +0200

    4. óra elejei feladat

---
+++ main.js
@@ -0,0 +1,13 @@
+$(function(){
+    $("#ok").on("click", function(){
+        $("article").html("<img src='kep.jpg' alt=''>")
+        $("img").css("border" , "8px solid red")
+        $("img").css("padding", "20px")
+
+    })
+
+})
+
+
+
+
